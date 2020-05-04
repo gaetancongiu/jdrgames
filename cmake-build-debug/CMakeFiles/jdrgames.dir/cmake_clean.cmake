b576a98cb8ea56bc7714cf19e1cce0314f742f19
congiu.gaetan <gaetan.congiu@esqese.ucly.fr>
2020-05-04 14:52:54 +0200

    "Initialisation du projet"

---
+++ cmake-build-debug/CMakeFiles/jdrgames.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jdrgames.dir/main.cpp.obj"
+  "bin/jdrgames.exe"
+  "bin/jdrgames.exe.manifest"
+  "bin/jdrgames.pdb"
+  "lib/jdrgames.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jdrgames.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
