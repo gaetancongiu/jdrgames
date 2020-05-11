78e2799b264a50a6ab56a45ebe6f0df13a0fc364
congiu.gaetan <gaetan.congiu@esqese.ucly.fr>
2020-05-11 14:02:42 +0200

    Création de la classe personnage

---
+++ Personnage.cpp
@@ -0,0 +1,5 @@
+//
+// Created by congiu.gaetan on 11/05/2020.
+//
+
+#include "Personnage.h"
---
+++ Personnage.h
@@ -0,0 +1,12 @@
+//
+// Created by congiu.gaetan on 11/05/2020.
+//
+
+#ifndef JDRGAMES__PERSONNAGE_H_
+#define JDRGAMES__PERSONNAGE_H_
+
+class Personnage {
+
+};
+
+#endif //JDRGAMES__PERSONNAGE_H_
